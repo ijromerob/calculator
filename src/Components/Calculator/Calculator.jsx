979336d395a68a7d27ca2a81ea1bba9c4b8b3e0b
ijromerob <ijromerob@gmail.com>
2025-02-13 15:48:06 -0700

    added calculator logic and connections with main components

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,5 +1,42 @@
-function Calculator() {
-  return <h1>Hello there</h1>;
+import { useState } from 'react';
+import { calculateAll } from '../../assets/logic';
+
+function Calculator({ setResult }) {
+  const [isMetric, setIsMetric] = useState(true);
+  const [distance, setDistance] = useState(0);
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          setResult(calculateAll(isMetric, e.target[1].value));
+          e.preventDefault();
+        }}
+      >
+        <label className="switch">
+          Units:
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              setIsMetric(!e.target.checked);
+            }}
+          />
+          <span className="slider round">{isMetric ? 'Metric' : 'Miles'}</span>
+        </label>
+        <label>
+          <input
+            type="number"
+            name="distance"
+            placeholder={isMetric ? 'Distance in Km' : 'Distance in Miles'}
+            onChange={(a) => setDistance(a.target.value)}
+            value={distance}
+            required
+          />
+        </label>
+        <button type="submit">Calculate</button>
+      </form>
+    </>
+  );
 }
 
 export default Calculator;
--- src/assets/logic.js
+++ src/assets/logic.js
@@ -21,12 +21,12 @@ function calculateCost(distance) {
 
 // function to calculate the CO2 emissions for each mode of transport
 function calculateEmissions(distance) {
-  // CO2 emissions per km for each mode of transport according to
+  // CO2 grams emissions per km for each mode of transport according to
   const emissions = {
-    Walking: 0,
-    Biking: 0,
-    Driving: 0.17,
-    Transit: 0.03,
+    Walking: 8,
+    Biking: 33,
+    Driving: 170,
+    Transit: 97,
   };
 
   const response = {};
@@ -37,3 +37,30 @@ function calculateEmissions(distance) {
 
   return response;
 }
+
+// convert emissions from grams to ounces
+
+function gramsCO2ToOuncesCO2(emissions) {
+  for (const mode in emissions) {
+    emissions[mode] = emissions[mode] / 28.3495;
+  }
+  return emissions;
+}
+
+function milesToKilometers(miles) {
+  let kilometers = miles * 1.60934;
+  return kilometers;
+}
+
+function calculateAll(isMetric, distance) {
+  distance = parseFloat(distance);
+  let distanceInKm = isMetric ? distance : milesToKilometers(distance);
+  const cost = calculateCost(distanceInKm);
+  const emissions = isMetric
+    ? calculateEmissions(distanceInKm)
+    : gramsCO2ToOuncesCO2(calculateEmissions(distanceInKm));
+
+  return { cost, emissions, isMetric, distance };
+}
+
+export { calculateAll };
